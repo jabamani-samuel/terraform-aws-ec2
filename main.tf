5b6eee2796f1b94ec350fde7942b57aab082965d
jabamani samuel <jabamani.samuel@intellectdesign.com>
2021-04-14 16:25:44 +0530

    main.tf updated without ssm

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "testbucketone"
+    bucket         = "testbucket"
     key            = "terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-db"
@@ -10,105 +10,25 @@ terraform {
 
 
 resource "aws_instance" "myInstance" {
-  ami           = "ami-05d72852800cbf29e"
+  ami           = "ami-0a91cd140a1fc148a"
   instance_type = "t2.micro"
-  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
   key_name = "deployer-one"
   user_data = <<-EOF
 	#! /bin/bash
-	sudo yum update -y
-	sudo yum install -y docker
-	sudo service docker start
+	sudo apt-get update
+        sudo apt-get upgrade
+        sudo apt-get -y install docker.io
+        sudo systemctl start docker
+        sudo systemctl enable docker
 	sudo docker run -p 8080:8080 somestupiddocker/terraform-aws-ec2-docker:latest
 	EOF
-	
-
 }		
-resource "aws_iam_role" "test_role" {
-  name = "ssm_role"
-
-assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-managed_policy_arns = [aws_iam_policy.policy_two.arn]
-
-  tags = {
-      tag-key = "tag-value"
-  }
-}
-
-resource "aws_iam_policy" "policy_two" {
-  name = "policy-381967"
-
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ssm:DescribeAssociation",
-                "ssm:GetDeployablePatchSnapshotForInstance",
-                "ssm:GetDocument",
-                "ssm:DescribeDocument",
-                "ssm:GetManifest",
-                "ssm:GetParameter",
-                "ssm:GetParameters",
-                "ssm:ListAssociations",
-                "ssm:ListInstanceAssociations",
-                "ssm:PutInventory",
-                "ssm:PutComplianceItems",
-                "ssm:PutConfigurePackageResult",
-                "ssm:UpdateAssociationStatus",
-                "ssm:UpdateInstanceAssociationStatus",
-                "ssm:UpdateInstanceInformation"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ssmmessages:CreateControlChannel",
-                "ssmmessages:CreateDataChannel",
-                "ssmmessages:OpenControlChannel",
-                "ssmmessages:OpenDataChannel"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2messages:AcknowledgeMessage",
-                "ec2messages:DeleteMessage",
-                "ec2messages:FailMessage",
-                "ec2messages:GetEndpoint",
-                "ec2messages:GetMessages",
-                "ec2messages:SendReply"
-            ],
-            "Resource": "*"
-        }
-    ]
-})
-}
-
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "ssm_profile"
-  role = aws_iam_role.test_role.name
-}
 
 provider "aws" {
   profile = "default"
   region  = "us-east-2"
 }
+
 
 
 module "key_pair" {
@@ -122,8 +42,9 @@ module "key_pair" {
 
 
 output "DNS" {
-  value = aws_instance.myInstance.id
+  value = aws_instance.myInstance.public_dns
 }
-output "state" {
-  value = aws_instance.myInstance.instance_state
+
+output "ID" {
+  value = aws_instance.myInstance.id
 }
