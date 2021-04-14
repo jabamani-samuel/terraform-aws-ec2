ba5890a39f9f72e6e2f427b8a685b442c2d3b899
jabamani samuel <jabamani.samuel@intellectdesign.com>
2021-04-14 15:46:00 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,129 @@
+terraform {
+  backend "s3" {
+    bucket         = "testbucketone"
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-db"
+    encrypt        = true
+  }
+}
+
+
+resource "aws_instance" "myInstance" {
+  ami           = "ami-05d72852800cbf29e"
+  instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+  key_name = "deployer-one"
+  user_data = <<-EOF
+	#! /bin/bash
+	sudo yum update -y
+	sudo yum install -y docker
+	sudo service docker start
+	sudo docker run -p 8080:8080 somestupiddocker/terraform-aws-ec2-docker:latest
+	EOF
+	
+
+}		
+resource "aws_iam_role" "test_role" {
+  name = "ssm_role"
+
+assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+managed_policy_arns = [aws_iam_policy.policy_two.arn]
+
+  tags = {
+      tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_policy" "policy_two" {
+  name = "policy-381967"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeAssociation",
+                "ssm:GetDeployablePatchSnapshotForInstance",
+                "ssm:GetDocument",
+                "ssm:DescribeDocument",
+                "ssm:GetManifest",
+                "ssm:GetParameter",
+                "ssm:GetParameters",
+                "ssm:ListAssociations",
+                "ssm:ListInstanceAssociations",
+                "ssm:PutInventory",
+                "ssm:PutComplianceItems",
+                "ssm:PutConfigurePackageResult",
+                "ssm:UpdateAssociationStatus",
+                "ssm:UpdateInstanceAssociationStatus",
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2messages:AcknowledgeMessage",
+                "ec2messages:DeleteMessage",
+                "ec2messages:FailMessage",
+                "ec2messages:GetEndpoint",
+                "ec2messages:GetMessages",
+                "ec2messages:SendReply"
+            ],
+            "Resource": "*"
+        }
+    ]
+})
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "ssm_profile"
+  role = aws_iam_role.test_role.name
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+
+module "key_pair" {
+
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name   = "deployer-one"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAiAistfEpeFZJQy9LvplAwVwPcALRbp7sN6E5Nw1Rf1/PQc8TFsfONo+FNXeqIuGbgZNpAglsNROyHQaldMDHlDF/GtUzsBpCplSYweDPcmhlLt9NToGqyZ+YA9VYzWdC20Sl/bajs9L3nuwJIaO0Gw7rbhbSMKwuGCJrNjJrvazj5yR5hvopJfdsWriCVekhdr/GqsKh651RE/vHRFzmPvlKUwciIHsYrt4sWv3Tl9PWnHT8uwx4xDE8KxEhD1ZaV66C8YPSpHearkSbpMLhXmMrU3GSS1Po108KBw2JYvNn7mIHNQ511Ag9bIkA/1TF9yHFlG8fVhjGWC8x5B/yvw=="
+
+}
+
+
+output "DNS" {
+  value = aws_instance.myInstance.id
+}
+output "state" {
+  value = aws_instance.myInstance.instance_state
+}
