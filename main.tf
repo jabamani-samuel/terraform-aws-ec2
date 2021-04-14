b39c63faf559211e01f74f2cac883cf9d500eacb
jabamani samuel <jabamani.samuel@intellectdesign.com>
2021-04-14 22:20:50 +0530

    destroy added

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
 resource "aws_instance" "myInstance" {
   ami           = "ami-05d72852800cbf29e"
   instance_type = "t2.micro"
-  key_name = "t2keypair"
+  key_name = "deployer-one"
   user_data = <<-EOF
 	#! /bin/bash
 	echo hello
